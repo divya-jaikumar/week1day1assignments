8b2dd215ae90af82441a4d5e5255fa4fa93565af
divya-jaikumar <96763837+divya-jaikumar@users.noreply.github.com>
2022-01-06 18:56:50 +0530

    Add files via upload

---
+++ day1assignment/Factorial.java
@@ -0,0 +1,13 @@
+package week1.day1assignment;
+
+
+	public class Factorial {
+		public static void main(String[] args) {
+			int fact =1;
+			int input=5;
+			for(int i=1;i<=input;i++) {
+				fact=fact *i;      
+			}
+			System.out.println(fact);
+		}
+	}
---
+++ day1assignment/PrintOdd.java
@@ -0,0 +1,12 @@
+package week1.day1assignment;
+
+public class PrintOdd {
+	public static void main(String[] args) {
+		for(int i=1;i<=10;i++) {
+			if(i%2==1) {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
